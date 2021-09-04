8955f2a92fe9fff83f2306c0ca6b795a4f885a37
gitbit789 <28954292+gitbit789@users.noreply.github.com>
2021-09-04 16:58:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ function compute() {
     var n = d.getFullYear();
     var futureDate = Number(n) + Number(years); 
 
-    document.getElementById("response1").innerHTML = "If you deposit $" + principal;
-    document.getElementById("response2").innerHTML = "At a rate of " + interestRate + '%';
-    document.getElementById("response3").innerHTML = "You'll receive the amount of $" + interestEarned
-    document.getElementById("response4").innerHTML = "In the year " + futureDate
+
+
+    document.getElementById("response1").innerHTML = "Please enter a positive number.";
+    document.getElementById("response2").innerHTML = "If you deposit $" + principal;
+    document.getElementById("response3").innerHTML = "At a rate of " + interestRate + '%';
+    document.getElementById("response4").innerHTML = "You'll receive the amount of $" + interestEarned
+    document.getElementById("response5").innerHTML = "In the year " + futureDate
 }    
